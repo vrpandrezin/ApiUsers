0b037c3758a76ef081a81b04c797e70231f53127
André Matias <andre.matias@araujo.com.br>
2024-11-19 10:15:12 -0300

    Alterações no UserModal

---
+++ src/main/java/br/com/matias/todolist/modal/UserModal.java
@@ -0,0 +1,32 @@
+package br.com.matias.todolist.modal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_users")
+public class UserModal {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    private String name;
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String username;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
