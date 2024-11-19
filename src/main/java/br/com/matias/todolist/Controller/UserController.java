66da4fd08cd4db101f3a32c47840955de00d63af
André Matias <andre.matias@araujo.com.br>
2024-11-19 10:39:56 -0300

    Criação de endpoints para listar, atualizar, deletar e adicionar usuários.

--- src/main/java/br/com/matias/todolist/Controller/UserController.java
+++ src/main/java/br/com/matias/todolist/Controller/UserController.java
@@ -1,15 +1,16 @@
-package br.com.matias.todolist.Controller;
+package br.com.matias.todolist.controller;
 
 import br.com.matias.todolist.Response.ErroResponse;
 import br.com.matias.todolist.Response.StatusResponse;
-import br.com.matias.todolist.Service.UserModalService;
-import br.com.matias.todolist.Service.UserService;
+import br.com.matias.todolist.modal.DTO.UserDTO;
+import br.com.matias.todolist.modal.UserModal;
+import br.com.matias.todolist.service.UserService;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -19,26 +20,47 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/addUser")
-    public ResponseEntity<?> create(@RequestBody UserModalService userModalService) {
-        ErroResponse erro = userService.validaDadosDeCriacaoDoUsuario(userModalService);
-        if (erro != null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+    @GetMapping("/userList")
+    public ResponseEntity<?> listUsers() {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(new StatusResponse("Listagem de usuários", userService.findUsers()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErroResponse("Não foi possível listar os usuários."));
         }
-
-        UserModalService userCreated = userService.createUser(userModalService);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+    @PostMapping("/addUser")
+    public ResponseEntity<?> createUser(@RequestBody UserModal userModal) {
+        try {
+            UserDTO user = userService.createUser(userModal);
+            if (user != null) {
+                return ResponseEntity.status(HttpStatus.CREATED).body(new StatusResponse("Usuário cadastrado.", user));
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErroResponse("Esse email já foi cadastrado."));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErroResponse("Não foi possível cadastrar o usuário."));
+        }
+    }
+    @DeleteMapping("/remUser/{userID}")
+    public ResponseEntity<?> deleteUser(@PathVariable UUID userID) {
+        try {
+            if (userService.deletaUsuarioDoBanco(userID)) {
+                return ResponseEntity.status(HttpStatus.OK).body(new StatusResponse("Usuário deletado.", "O usuário foi excluído."));
+            }
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new StatusResponse("Erro ao deletar usuário.", "O id informado não existe no Banco de Dados."));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErroResponse("Não foi possível deletar o usuário."));
+        }
     }
 
-    @DeleteMapping("/remUser/{userId}")
-    public ResponseEntity<?> deleteUser(@PathVariable UUID userId) {
-        Optional<UserModalService> userOptional = userService.deletaUsuarioDoBanco(userId);
-        if (userOptional.isPresent()) {
-            return ResponseEntity.status(HttpStatus.OK)
-                                 .body(new StatusResponse("Usuário deletado."));
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                                 .body(new ErroResponse("ID não encontrado."));
+    @PutMapping("/updateUser/{userID}")
+    public ResponseEntity<?> updateUser(@PathVariable UUID userID, @RequestBody UserModal userModal) {
+        Object response = userService.atualizaDadosDoUsuario(userID, userModal);
+
+        if (response instanceof ErroResponse) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        } else if (response == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErroResponse("Usuário não encontrado."));
         }
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
