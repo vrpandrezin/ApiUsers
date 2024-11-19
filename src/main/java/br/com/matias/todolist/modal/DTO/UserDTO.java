94b2ed234192bd5b638248a22f75392cbfd151df
André Matias <andre.matias@araujo.com.br>
2024-11-19 10:14:26 -0300

    Criação userDTO.

---
+++ src/main/java/br/com/matias/todolist/modal/DTO/UserDTO.java
@@ -0,0 +1,19 @@
+package br.com.matias.todolist.modal.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private UUID id;
+    private String name;
+    private String username;
+    private String email;
+}
