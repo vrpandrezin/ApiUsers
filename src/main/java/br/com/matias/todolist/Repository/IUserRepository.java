fb598f911008472212ff5713446b44c44bcc7e34
André Matias <andre.matias@araujo.com.br>
2024-11-19 10:13:23 -0300

    Correção no repositório.

--- src/main/java/br/com/matias/todolist/Repository/IUserRepository.java
+++ src/main/java/br/com/matias/todolist/Repository/IUserRepository.java
@@ -1,13 +1,13 @@
-package br.com.matias.todolist.Repository;
+package br.com.matias.todolist.repository;
 
-import br.com.matias.todolist.Service.UserModalService;
+import br.com.matias.todolist.modal.UserModal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface IUserRepository extends JpaRepository<UserModalService, UUID> {
-    UserModalService findByUsername(String username);
-    UserModalService findByEmail(String email);
+public interface IUserRepository extends JpaRepository<UserModal, UUID> {
+    UserModal findByUsername(String username);
+    UserModal findByEmail(String email);
 }
