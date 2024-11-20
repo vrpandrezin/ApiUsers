242c6280cecc129b3a9f8ebd831a2fdb69e48eea
André Matias <andre.matias@araujo.com.br>
2024-11-19 21:50:22 -0300

    Tratamento de argumentos inválidos.

---
+++ src/main/java/br/com/matias/todolist/Response/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package br.com.matias.todolist.Response.Exception;
+
+import br.com.matias.todolist.Response.ErroResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErroResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErroResponse("Requisição inválida. Verifique o formato da URL ou do ID enviado."));
+    }
+}
